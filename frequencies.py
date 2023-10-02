e9aec7c3fb4c61ce8c80663d398b6e277aa2f4ad
Emily <k21073231@kcl.ac.uk>
2023-10-02 10:10:51 +0100

    First commit

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,19 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if isinstance(items,str):
+            key = item 
+        else:
+            key = str(item)
+        
+        if key in frequencies:
+            frequencies[key] += 1 
+        else:
+            frequencies[key] = 1
+
     return frequencies
+
+#Test cases 
+print(frequencies(['a','a','b','b','c']))
+print(frequencies([100,'Hello','100','100',100]))
